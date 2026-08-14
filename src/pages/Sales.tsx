a2ad5b42e83f0a2e92275e94227281f762421344
Kory <kory.tashim@icloud.com>
2026-08-14 21:51:08 +0500

    feat: added a modal window to sales.tsx

--- src/pages/Sales.tsx
+++ src/pages/Sales.tsx
@@ -1,11 +1,53 @@
 import './Sales.css'
 import {sales} from "../data/sales"
+import Modal from 'react-modal';
+import {useState} from "react";
+
+Modal.setAppElement("#root");
 
 const Sales = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [inputValue, setInputValue] = useState("");
+    const [sum, setSum] = useState("");
+    const [date,setDate] = useState("");
+
+    const openModal = () =>{
+        setModalIsOpen(true);
+    }
+
+    const closeModal = () =>{
+        setModalIsOpen(false);
+    }
+
+    const handleAdd = () => {
+        sales.unshift({
+            id: sales.length + 1,
+            name: inputValue,
+            sum: Number(sum),
+            date: date
+        });
+
+        closeModal();
+    }
+    
+        const modalContent = (
+            <div>
+                <h2>Добавьте товар</h2>
+                <input type="text" value={inputValue} onChange={event => setInputValue(event.target.value)} placeholder="Названия товара"/><br/>
+                <input type="number" value={sum} onChange={event => setSum(event.target.value)} placeholder="Сумма"/><br/>
+                <input type='date' value={date} onChange={event => setDate(event.target.value)} placeholder='Дата'/><br/>
+                <button onClick={handleAdd}>Добавить</button>
+                <button onClick={closeModal}>Закрыть</button>
+            </div>
+        );
+
     return(
         <>
         <h1>Продажи</h1>
-        <button>+ Добавить операцию</button>
+        <button onClick={openModal}>+ Добавить операцию</button>
+        <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className={"modal"} overlayClassName="modal-overlay">
+            {modalContent}
+        </Modal>
 
         <table>
             <thead>

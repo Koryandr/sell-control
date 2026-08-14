00cada259cd75d7cc3761d329194556b1a29b663
Kory <kory.tashim@icloud.com>
2026-08-14 18:04:08 +0500

    feat: added a modal window to products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,12 +1,35 @@
+import React, {useState} from "react";
 import "./Products.css"
 import { products } from "../data/products";
+import Modal from 'react-modal';
 
 const Products = () => {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const openModal = () =>{
+        setModalIsOpen(true);
+    }
+
+    const closeModal = () =>{
+        setModalIsOpen(false);
+    }
+
+    const modalContent = (
+        <div>
+            <h2>Заголовок модального окна</h2>
+            <p>Текст модального окна</p>
+            <button onClick={closeModal}>Закрыть</button>
+        </div>
+    );
 
     return(
         <>
         <h1>Товары</h1>
-        <button>+ Добавить товар</button>
+        <button onClick={openModal}>+ Добавить товар</button>
+        <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className={"modal"} overlayClassName="modal-overlay">
+            {modalContent}
+        </Modal>
 
         <table>
             <thead>

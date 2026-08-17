f74a00aac7ca148c36b6c6fee590fc4cced4847c
Kory <kory.tashim@icloud.com>
2026-08-17 19:40:17 +0500

    feat: added delete logic to sales
    Перевёл управление массивом продаж на React State.
    Добавил логику удаления продаж с подтверждением через модальное окно.
    Добавил отображение названия выбранного товара в окне подтверждения.
    Следующий этап — добавить редактирование продаж.

--- src/pages/Sales.tsx
+++ src/pages/Sales.tsx
@@ -1,5 +1,5 @@
 import './Sales.css'
-import {sales} from "../data/sales"
+import { sales as initialSales } from "../data/sales";
 import Modal from 'react-modal';
 import {useState} from "react";
 
@@ -10,6 +10,29 @@ const Sales = () => {
     const [inputValue, setInputValue] = useState("");
     const [sum, setSum] = useState("");
     const [date,setDate] = useState("");
+    const [idDelete,setIdDelete] = useState(0);
+    const [deleteModalIsOpen,setDeleteModalIsOpen] = useState(false);
+    const [sales,setSales] = useState(initialSales);
+    const [name,setName] = useState("");
+
+    const handleDelete = (id: number,name: string) =>{
+        setIdDelete(id);
+        setName(name);
+        openDeleteModal();
+    }
+
+    const confirmDelete = () =>{
+        setSales(sales.filter(sale => sale.id !== idDelete))
+        closeDeleteModal();
+    }
+
+    const openDeleteModal = () =>{
+        setDeleteModalIsOpen(true);
+    }
+
+    const closeDeleteModal = () =>{
+        setDeleteModalIsOpen(false);
+    }
 
     const openModal = () =>{
         setModalIsOpen(true);
@@ -20,12 +43,12 @@ const Sales = () => {
     }
 
     const handleAdd = () => {
-        sales.unshift({
+        setSales([{
             id: sales.length + 1,
             name: inputValue,
             sum: Number(sum),
-            date: date
-        });
+            date: date,
+        }, ...sales])
 
         closeModal();
     }
@@ -41,12 +64,24 @@ const Sales = () => {
             </div>
         );
 
+        const deleteModalContent = (
+            <div>
+                <h2>Вы действительно хотите удалить товар "{name}"?</h2>
+                <button onClick={confirmDelete}>Удалить</button>
+                <button onClick={closeDeleteModal}>Отмена</button>
+            </div>
+        )
+
     return(
         <>
         <h1>Продажи</h1>
         <button onClick={openModal}>+ Добавить операцию</button>
+        
         <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className={"modal"} overlayClassName="modal-overlay">
             {modalContent}
+        </Modal>
+        <Modal isOpen={deleteModalIsOpen} onRequestClose={closeDeleteModal} className={"modal"} overlayClassName="modal-overlay">
+            {deleteModalContent}
         </Modal>
 
         <table>
@@ -55,6 +90,7 @@ const Sales = () => {
                     <th>Товар</th>
                     <th>Продажи</th>
                     <th>Дата</th>
+                    <th>Действия</th>
                 </tr>
             </thead>
             <tbody>
@@ -63,6 +99,7 @@ const Sales = () => {
                         <td>{sale.name}</td>
                         <td>{sale.sum}</td>
                         <td>{sale.date}</td>
+                        <button onClick={() =>{handleDelete(sale.id,sale.name)}}>🗑️</button>
                     </tr>
                 ))}
             </tbody>
